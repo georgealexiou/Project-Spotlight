7fca51d4bb4b08dc747d6340df8e4ffa162de911
georgealexiou <george@alexiou.xyz>
2020-02-08 16:00:25 +0000

    Can now change colors

--- Desktop/mouse.py
+++ Desktop/mouse.py
@@ -1,10 +1,17 @@
 from pynput.mouse import Button, Controller
 import time
 
+import win32api
+import time
+import win32gui
+import win32con
+import ctypes
+
 class Mouse:
 
     def _init_():
         self.controller = Controller()
+        win_cursor_red()
 
     # prints pointer position
     def debug(self):
@@ -16,15 +23,17 @@ class Mouse:
         print('Mouse moved to {0}'.format(mouse.position))
 
     # move pointer relative to current position
-    def move(self):
-        mouse.move(5, -5)
+    def move(self, x, y):
+        mouse.move(x, y)
 
     # left click the mouse
     def left_click(self):
         mouse.click(Button.left, 1)
+        win_cursor_blue()
     
     def right_click(self):
         mouse.click(Button.right, 1)
+        win_cursor_blue()
 
     # press mouse for x amount of seconds
     def press(self, seconds):
@@ -33,8 +42,35 @@ class Mouse:
         mouse.release(Button.left)
 
     # scroll mouse, ussed for changing slides
-    # * dy > 0 ---> scroll down
-    # * dy < 0 ---> scroll up
-    # * dx = horizontal scrolling (not needed)
+    #* dy > 0 ---> scroll down
+    #* dy < 0 ---> scroll up
+    #* dx = horizontal scrolling (not needed)
     def scroll(self, dx, dy):
         mouse.scroll(dx, dy)
+
+    #! idk if this works we need to test it
+    #change to red
+    def win_cursor_red():
+        hold = win32gui.LoadImage(0, 32512, win32con.IMAGE_CURSOR, 0, 0, win32con.LR_SHARED )
+        hsave = ctypes.windll.user32.CopyImage(hold, win32con.IMAGE_CURSOR, 0, 0, win32con.LR_COPYFROMRESOURCE)
+        hnew = win32gui.LoadImage(0, 'cursor_files/mouse_up_cursor.cur', win32con.IMAGE_CURSOR, 0, 0, win32con.LR_LOADFROMFILE);
+        ctypes.windll.user32.SetSystemCursor(hnew, 32512)
+    
+    #change to blue
+    def win_cursor_blue():
+        hold = win32gui.LoadImage(0, 32512, win32con.IMAGE_CURSOR, 0, 0, win32con.LR_SHARED )
+        hsave = ctypes.windll.user32.CopyImage(hold, win32con.IMAGE_CURSOR, 0, 0, win32con.LR_COPYFROMRESOURCE)
+        hnew = win32gui.LoadImage(0, 'cursor_files/mouse_down_cursor.cur', win32con.IMAGE_CURSOR, 0, 0, win32con.LR_LOADFROMFILE);
+        ctypes.windll.user32.SetSystemCursor(hnew, 32512)
+
+    # change to up arrow
+    def win_cursor_scroll_up():
+        pass
+
+    # change to down arrow
+    def win_cursor_scroll_down():
+        pass
+
+    #reset to normal
+    def win_cursor_reset():
+        ctypes.windll.user32.SetSystemCursor(hsave, 32512)
