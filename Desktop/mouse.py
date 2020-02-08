6ca0996c8518128926416ee442689ac2327eb27e
georgealexiou <george@alexiou.xyz>
2020-02-08 15:44:42 +0000

    Converted to object

--- Desktop/mouse.py
+++ Desktop/mouse.py
@@ -1,41 +1,40 @@
 from pynput.mouse import Button, Controller
-#import pyautogui
 import time
 
-mouse = Controller()
+class Mouse:
 
-# screen resolution
-#width, height = pyautogui.size()
+    def _init_():
+        self.controller = Controller()
 
-# prints pointer position
-def debug():
-    print("Mouse is at {0}".format(mouse.position))
+    # prints pointer position
+    def debug(self):
+        print("Mouse is at {0}".format(mouse.position))
 
-# sets pointer position
-def set_position(x, y):
-    mouse.position = (x, y)
-    print('Mouse moved to {0}'.format(mouse.position))
+    # sets pointer position
+    def set_position(self, x, y):
+        mouse.position = (x, y)
+        print('Mouse moved to {0}'.format(mouse.position))
 
-# move pointer relative to current position
-def move():
-    mouse.move(5, -5)
+    # move pointer relative to current position
+    def move(self):
+        mouse.move(5, -5)
 
-# click the mouse
-def click():
-    mouse.click(Button.left, 1)
+    # left click the mouse
+    def left_click(self):
+        mouse.click(Button.left, 1)
+    
+    def right_click(self):
+        mouse.click(Button.right, 1)
 
-# press mouse for x amount of seconds
-def press(seconds):
-    mouse.press(Button.left)
-    time.sleep(seconds)
-    mouse.release(Button.left)
+    # press mouse for x amount of seconds
+    def press(self, seconds):
+        mouse.press(Button.left)
+        time.sleep(seconds)
+        mouse.release(Button.left)
 
-# scroll mouse, ussed for changing slides
-# * dy > 0 ---> scroll down
-# * dy < 0 ---> scroll up
-# * dx = horizontal scrolling (not needed)
-def scroll(dx, dy):
-    mouse.scroll(dx, dy)
-
-time.sleep(3)
-scroll(0,1)
+    # scroll mouse, ussed for changing slides
+    # * dy > 0 ---> scroll down
+    # * dy < 0 ---> scroll up
+    # * dx = horizontal scrolling (not needed)
+    def scroll(self, dx, dy):
+        mouse.scroll(dx, dy)
