8b4cc6e86352588792faae3788008f227d72f1ff
georgealexiou <george@alexiou.xyz>
2020-02-08 19:37:13 +0000

    Changed format of packet parser

--- Desktop/packet_parser.py
+++ Desktop/packet_parser.py
@@ -12,27 +12,27 @@ def parse(message):
     key = message.split(":")[0]
     value = message.split(":")[1]
 
-    try
-        # clicks
-        if key == 'Mouse_Right':
-            if value == 'Click':
-                mouse.right_click()
-        elif message == 'Mouse_Left':
-            if value == 'Click':
-                mouse.left_click()
+    # clicks
+    if key == 'Mouse_Right':
+        if value == 'Click':
+            mouse.right_click()
+    elif message == 'Mouse_Left':
+        if value == 'Click':
+            mouse.left_click()
         
-        # scrolling
-        #TODO: may have to change this to support faster scrolling
-        elif message == 'scroll_down':
+    # scrolling
+    #TODO: may have to change this to support faster scrolling
+    elif key == 'Mouse_Scroll':
+        if value == 'Down':
             mouse.scroll(0, 1)
-        elif  message == 'scroll_up':
+        elif value == 'Up':
             mouse.scroll(0, -1)
         
-        # mouse movement
-        elif message.contains('Mouse_Coordinates'):
-            x = message.split(':')[1].split * screen_width
-            y = message.split('_')[2] * screen_height
-        
-        # error case
-        else:
-            print ('Invalid message - packet ignored')
+    # mouse movement
+    elif key == 'Mouse_Move':
+        x = value.split(',')[0].split * screen_width
+        y = message.split(',')[1] * screen_height
+    
+    # error case
+    else:
+        print ('Invalid message - packet ignored')
